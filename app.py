dec17d94d86bc3a77855b9db47b9908dc592ed40
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2022-02-24 17:51:55 +0900

    스택오버플로우 스크래퍼

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request
 
-app = Flask("scrapper!!")
+from soScraper import get_last_page, get_so_jobs
+
+app = Flask("scraper!!")
 
 
 @app.route("/")
 def home():
+
     return render_template("home.html")
+
+
+@app.route("/search")
+def search():
+    word = request.args.get("word").lower()
+    if word == "":
+        return redirect("/")
+    get_so_jobs(word)
+
+    return render_template("search.html", word=word)
---
+++ soScraper.py
@@ -0,0 +1,54 @@
+from unittest import result
+from bs4 import BeautifulSoup
+import requests
+
+# 직업,회사,링크(https://stackoverflow.com/jobs/:id)
+
+
+def get_last_page(url):
+    i = 1
+    for i in range(1, 20):
+        result = requests.get(url+f"&pg={i}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        pages = soup.find(
+            "div", class_="s-pagination").find_all("a")[-1].find("span").text
+        i += 1
+        if(pages != "next"):
+            i -= 1
+            break
+        elif i == 20:
+            break
+
+    return i
+
+
+def extract_job(result):
+    job_id = result["data-jobid"]
+    title = result.find("a", {"title": True}).text
+    company = result.find("h3", class_="mb4").find_all("span")[
+        0].get_text(strip=True)
+    link = f"https://stackoverflow.com/jobs/{job_id}"
+    return {
+        "title": title,
+        "company": company,
+        "link": link
+    }
+
+
+def extract_jobs(url, last_page):
+    jobs = []
+    for page in range(last_page):
+        result = requests.get(url+f"&pg={page}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("div", class_="-job")
+        for result in results:
+            job = extract_job(result)
+            jobs.append(job)
+    return jobs
+
+
+def get_so_jobs(word):
+    url = f"https://stackoverflow.com/jobs?r=true&q={word}"
+    last_page = get_last_page(url)
+    jobs = extract_jobs(url, last_page)
+    return jobs
