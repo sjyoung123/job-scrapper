f5bb6f895b9e1fb30c108cf2a9c6dd5e1411c73f
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2022-02-25 17:37:36 +0900

    wwrScraper

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask, redirect, render_template, request
 from remoteScraper import get_remote_jobs
 
 from soScraper import get_last_page, get_so_jobs
+from wwrScraper import get_wwr_jobs
 
 app = Flask("scraper!!")
 
 
 @app.route("/")
 def home():
+    get_wwr_jobs()
     return render_template("home.html")
 
 
@@ -18,5 +20,6 @@ def search():
         return redirect("/")
     get_so_jobs(word)
     get_remote_jobs(word)
+    get_wwr_jobs(word)
 
     return render_template("search.html", word=word)
--- wwrScraper.py
+++ wwrScraper.py
@@ -1,5 +1,40 @@
-url = "https://weworkremotely.com/remote-jobs/search?term=python"
+from bs4 import BeautifulSoup
+import requests
+# 직업,회사,링크(https://weworkremotely.com/remote-jobs/:id)
 
 
-def get_wwr_jobs():
-    url = "https://weworkremotely.com/remote-jobs/search?term=python"
+def extract_job(soup):
+    job_id = soup.select_one("li > a")["href"]
+    title = soup.find("span", class_="title").text
+    company = soup.find("span", class_="company").text
+    link = f"https://weworkremotely.com/remote-jobs{job_id}"
+
+    return {
+        "title": title,
+        "company": company,
+        "link": link
+    }
+
+
+def extract_jobs(url):
+    jobs = []
+    header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0"
+              }  # 503에러 해결 => 브라우저에 봇이 아닌 사람이란 걸 알려주는 헤더 코드
+    result = requests.get(url, headers=header).text
+
+    soup = BeautifulSoup(result, "html.parser")
+    section_list = soup.find_all("section", class_="jobs")
+    for section in section_list:
+        job_list = section.find_all("li")
+        del job_list[-1]
+        for job_result in job_list:
+            job = extract_job(job_result)
+            jobs.append(job)
+    return jobs
+
+
+def get_wwr_jobs(word):
+    url = f"https://weworkremotely.com/remote-jobs/search?term={word}"
+    jobs = extract_jobs(url)
+    print(jobs)
+    return jobs
